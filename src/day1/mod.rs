2d59b4b3735386f6173896d351ad346aa48ea0f9
Felix Braeunling <felix.braeunling@ul.com>
2024-12-02 16:00:04 +0100

    Improve day 1 performance

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,4 +1,3 @@
-use anyhow::{bail, Result};
 use aoc_runner_derive::{aoc, aoc_generator};
 use fxhash::FxHashMap;
 use itertools::multiunzip;
@@ -10,61 +9,54 @@ type Output = i64;
 type Input = (Vec<i64>, Vec<i64>);
 
 #[aoc_generator(day1)]
-pub fn input_generator(input: &str) -> Result<Input> {
-    Ok(multiunzip(
+pub fn input_generator(input: &str) -> Input {
+    multiunzip(
         input
             .lines()
-            .map(|line| line.split_once("   "))
-            .map(|split| {
-                if let Some((first, second)) = split {
-                    if let (Ok(first), Ok(second)) = (first.parse::<i64>(), second.parse::<i64>()) {
-                        Ok((first, second))
-                    } else {
-                        bail!("At least one not parsable to i64: {}, {}", first, second)
-                    }
-                } else {
-                    bail!("No split found");
-                }
-            })
-            .collect::<Result<Vec<(i64, i64)>>>()?,
-    ))
+            .flat_map(|line| 
+                {line.split_whitespace()
+                .map(|n| n.parse::<i64>().unwrap())
+                .tuple_windows()
+                .map(|(a, b)| (a, b))
+                })
+            .collect::<Vec<(i64, i64)>>()
+    )
 }
 
 #[aoc(day1, part1)]
-pub fn solve_part1(input: &Input) -> Result<Output> {
+pub fn solve_part1(input: &Input) -> Output {
     let mut left = input.0.clone();
     let mut right = input.1.clone();
     left.par_sort_unstable();
     right.par_sort_unstable();
-    Ok(input
-        .0
+    left
         .iter()
-        .zip(input.1.iter())
+        .zip(right.iter())
         .map(|(a, b)| (a - b).abs())
-        .sum())
+        .sum()
 }
 
 #[aoc(day1, part2)]
-pub fn solve_part2(input: &Input) -> Result<Output> {
+pub fn solve_part2(input: &Input) -> Output {
     let num_count = input.1.iter().fold(FxHashMap::default(), |mut map, num| {
         map.entry(num)
             .and_modify(|frequency| *frequency += 1)
             .or_insert(1);
         map
     });
-    Ok(input
+    input
         .0
         .iter()
-        .map(|num| num_count.get(num).unwrap_or(&0))
-        .sum())
+        .map(|num| num * num_count.get(num).unwrap_or(&0))
+        .sum()
 }
 
 pub fn part1(input: &str) -> impl std::fmt::Display {
-    solve_part1(&input_generator(input).unwrap()).unwrap()
+    solve_part1(&input_generator(input))
 }
 
 pub fn part2(input: &str) -> impl std::fmt::Display {
-    solve_part2(&input_generator(input).unwrap()).unwrap()
+    solve_part2(&input_generator(input))
 }
 
 #[cfg(test)]
@@ -85,15 +77,15 @@ mod tests {
     fn samples_part1() {
         assert_eq!(
             11,
-            solve_part1(&input_generator(sample()).unwrap()).unwrap()
+            solve_part1(&input_generator(sample()))
         );
     }
 
     #[test]
     fn samples_part2() {
         assert_eq!(
-            11,
-            solve_part1(&input_generator(sample()).unwrap()).unwrap()
+            31,
+            solve_part2(&input_generator(sample()))
         );
     }
 }
